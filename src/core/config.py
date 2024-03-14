258f5c619a948dbe1c8f3b72b2ef97f6b8cf84f4
George Waters <gwatersdev@gmail.com>
2024-03-14 16:25:49 -0400

    Use resolve instead of absolute for paths

--- src/core/config.py
+++ src/core/config.py
@@ -5,7 +5,6 @@ import json, logging, sys
 DEFAULT_BACKUP_CONFIG = "drive-backup.bkp"
 DEFAULT_LOG = "drive-backup.log"
 
-# TODO: Add option to delete user credentials (token)
 
 class Config:
     def __init__(self, args=None):
@@ -15,9 +14,9 @@ class Config:
 
     def update_values(self, args):
         if args.get("backup_config") is True:
-            self.backup_config = Path().absolute() / DEFAULT_BACKUP_CONFIG
+            self.backup_config = Path().resolve() / DEFAULT_BACKUP_CONFIG
         elif "backup_config" in args:
-            self.backup_config = Path(args["backup_config"]).absolute()
+            self.backup_config = Path(args["backup_config"]).resolve()
             if self.backup_config.is_dir():
                 self.backup_config = self.backup_config / DEFAULT_BACKUP_CONFIG
         else:
@@ -27,19 +26,19 @@ class Config:
             config_args = self.load_config_json(self.backup_config)
             args = config_args | args
 
-        self.destination = Path(args.get("destination", "")).absolute()
+        self.destination = Path(args.get("destination", "")).resolve()
         self.backup_name = args.get("backup_name")
         self.backup_type = args.get("backup_type", "complete")
         self.prev_backup_name = args.get("prev_backup_name")
         self.source = args.get("source")
         self.source_id = args.get("source_id", "root")
         self.google_doc_mimeType = args.get("google_doc_mimeType", "msoffice")
-        self.client_credentials = Path(args["client_credentials"]).absolute() if args.get("client_credentials") else None
+        self.client_credentials = Path(args["client_credentials"]).resolve() if args.get("client_credentials") else None
         self.log_level = args.get("log_level", "INFO")
         self.log_filter = bool(args.get("log_filter", False))
         self.log_changes = bool(args.get("log_changes", False))
         if args.get("log_path"):
-            self.log_path = Path(args["log_path"]).absolute()
+            self.log_path = Path(args["log_path"]).resolve()
             if self.log_path.is_dir():
                 self.log_path = self.log_path / DEFAULT_LOG
         else:
