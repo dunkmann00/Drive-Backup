f88e09815dfce8c04bb6e98ff4630fcd9abe55c8
George Waters <gwatersdev@gmail.com>
2024-04-11 20:03:00 -0400

    Allow more customization in app-build.py

--- app-build.py
+++ app-build.py
@@ -6,6 +6,7 @@ import subprocess, platform, shutil, sys
 PYINSTALLER_BUILD_COMMAND = """
 pyinstaller src/drive_backup/__main__.py
     --name "dbackup"
+    --distpath {distpath}
     --additional-hooks-dir hooks
     --clean
     --noconfirm
@@ -14,6 +15,8 @@ pyinstaller src/drive_backup/__main__.py
 PROJECT_NAME = "Drive Backup"
 NOTIFICATIONS_PROJECT_NAME = "Drive Backup Notifications"
 
+DEFAULT_DISTPATH = Path.cwd() / 'dist'
+
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.group(context_settings=CONTEXT_SETTINGS, help=f"Build/Archive {PROJECT_NAME} app binary.")
@@ -21,9 +24,10 @@ def cli():
     pass
 
 @cli.command()
+@click.option("--distpath", default=str(DEFAULT_DISTPATH), help="The location to put the binary build. (Default: './dist')")
 @click.option("--macos-codesign-identity", envvar="MACOS_CODESIGN_IDENTITY", help="Sign the app with the provided identity (macOS only).")
 @click.option("--macos-target-arch", default="universal2", help="The target architecture to build for (macOS only).")
-def build(macos_codesign_identity, macos_target_arch):
+def build(distpath, macos_codesign_identity, macos_target_arch):
     print("Setting up PyInstaller build...", file=sys.stderr)
     build_command = PYINSTALLER_BUILD_COMMAND
     if platform.system() == "Darwin":
@@ -32,20 +36,25 @@ def build(macos_codesign_identity, macos_target_arch):
         if macos_codesign_identity is not None:
             print("Adding '--codesign-identity' for macOS build.", file=sys.stderr)
             build_command += f' --codesign-identity "{macos_codesign_identity}"'
-    ret_code = subprocess.run(split(build_command)).returncode
+    ret_code = subprocess.run(split(build_command.format(distpath=distpath))).returncode
     if ret_code != 0:
         return ret_code
     print(f"Rename app directory to '{PROJECT_NAME}'.", file=sys.stderr)
-    app_path = Path.cwd() / "dist" / "dbackup"
-    app_path.rename(app_path.parent / PROJECT_NAME)
+    app_path = Path(distpath).resolve() / "dbackup"
+    app_path = app_path.rename(app_path.parent / PROJECT_NAME)
+    print(f"Binary created at '{app_path}'", file=sys.stderr)
+    print(str(app_path))
 
 @cli.command()
-def add_notifications():
+@click.option("--app-path", default=str(DEFAULT_DISTPATH / PROJECT_NAME),
+    help="The location of the binary build directory. (Default: './dist/Drive Backup')"
+)
+def add_notifications(app_path):
     if platform.system() in ["Darwin", "Windows"]:
         platform_dir = "mac" if platform.system() == "Darwin" else "windows"
         relative_pkg_path = Path("drive_backup") / "core" / "notifications" / platform_dir / "build"
         src_path = Path.cwd() / "src" / relative_pkg_path
-        dst_path = Path.cwd() / "dist" / PROJECT_NAME / "_internal" / relative_pkg_path
+        dst_path = Path(app_path) / "_internal" / relative_pkg_path
         if platform.system() == "Darwin":
             print("Copying macOS notifications app into app build.", file=sys.stderr)
             src_path = src_path / f"{NOTIFICATIONS_PROJECT_NAME}.app"
@@ -57,13 +66,24 @@ def add_notifications():
             shutil.copy2(src_path, dst_path)
 
 @cli.command()
-@click.option("--name", default=PROJECT_NAME.replace(" ","_"), help="The name of the archive file to create.")
+@click.option("--app-path", default=str(DEFAULT_DISTPATH / PROJECT_NAME),
+    help="The location of the binary build directory. (Default: './dist/Drive Backup')"
+)
+@click.option("--archive-path", default=str(DEFAULT_DISTPATH / PROJECT_NAME.replace(" ","_")),
+    help="The location to create the archive file, minus any format-specific extension. (Default: './dist/Drive_Backup')"
+)
+@click.option("--archive-name",
+    help="The name of the archive file. This will replace the final path component of 'archive_path' if given."
+)
 @click.option("--format", help="Force a specific archive format to be used. (Default: zip on Windows, gztar otherwise)")
-def archive(name, format):
-    app_path = Path.cwd() / "dist" / name
+def archive(app_path, archive_path, archive_name, format):
+    app_path = Path(app_path).resolve()
+    archive_path = Path(archive_path).resolve()
+    if archive_name is not None:
+        archive_path = archive_path.parent / archive_name
     format = format or ('zip' if platform.system() == 'Windows' else 'gztar')
     print(f"Creating a {format} archive of the app.", file=sys.stderr)
-    archive_name = shutil.make_archive(app_path, format, root_dir=app_path.parent, base_dir=app_path.name)
+    archive_name = shutil.make_archive(str(archive_path), format, root_dir=str(app_path.parent), base_dir=app_path.name)
     print(f"Archive created at '{archive_name}'", file=sys.stderr)
     print(archive_name)
 
@@ -76,7 +96,7 @@ def archive_name(version):
     if project_platform == "darwin":
         project_platform = "macos"
         project_arch = "universal2"
-    return f"{project_name}-{version}-{project_arch}"
+    print(f"{project_name}-{version}-{project_arch}")
 
 if __name__ == '__main__':
     cli()
